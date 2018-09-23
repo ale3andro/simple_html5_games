c8baf0c05b163d3f3b4e60220affd0e19b89290c
ale3andro <ale3andro@gmail.com>
2018-09-23 21:11:17 +0300

    Προσωρινό commit

--- index2.js
+++ index2.js
@@ -1,4 +1,7 @@
 var debugging = true;
+var metadata = [];
+var draggables = [];
+var droppables = [];
 
 /** FROM https://www.kevinleary.net/javascript-get-url-parameters/
  * JavaScript Get URL Parameter
@@ -27,7 +30,7 @@ function get_data_from_server(){
     $.ajaxSetup({
         async: false
     });
-    var jqxhr = $.getJSON("quizes/" + getUrlParams('id') + ".json", function(data) {
+    var jqxhr = $.getJSON("matching/" + getUrlParams('id') + ".json", function(data) {
         if (debugging)
             console.log("Η φόρτωση του json από τον server ολοκληρώθηκε με επιτυχία");
             parse_quiz(data);
@@ -40,60 +43,64 @@ function get_data_from_server(){
     });
     
     function parse_quiz(data) {
-        var read_type="question"; // Διαβάζει ερώτηση του quiz
-        var read_type="metadata"; // Διαβάζει metadata του quiz
-        var read_type="0";
-        var question = '';
-        var question_count=0;
-        var metadata = '';
-    
-        $.each(data, function(index, value) {
-            read_type = "0"; // Πριν αρχίσει το διάβασμα το ρυθμίζω σε μηδέν
-            question = '';
-            $.each(value, function(key, valu) {
-                if (read_type=="0") {
-                    if (key=="id") {
-                        read_type="metadata";
-                    } else {
-                        read_type="question";
-                    }   
-                }
-                if (read_type=="question") {
-                    if (question=='')
-                        question = valu;
-                    else    
-                        question = question + divider + valu;
-                } else {
-                    if (metadata=='')
-                        metadata=valu;
-                    else
-                        metadata = metadata + divider + valu;
-                }
-            });
-            if (read_type=="question") {
-                sessionStorage.setItem("question" + question_count, question);
-                /*
-                    status=0 --> not answered yet
-                    status=1 --> correct answer
-                    status=2 --> wrong answer
-                */
-                sessionStorage.setItem("question" + question_count + "_status", 0);
-                question_count+=1; 
-            } else
-                sessionStorage.setItem("metadata", metadata);
+        var draggables_count = 0;
+        var droppables_count = 0;
+           
+       metadata[0] = data['metadata']['title'];
+       metadata[1] = data['metadata']['class'];
+       metadata[2] = data['metadata']['answer'];
+        $.each(data['draggables'], function(index, value) {
+            draggables[draggables_count] = value;
+            draggables_count++;  
         });
-        sessionStorage.setItem("numQuestions", question_count);
+        console.log(draggables);
+        $.each(data['droppables'], function(index, value) {
+            droppables[droppables_count] = value;
+            droppables_count++;  
+        });
+        console.log(droppables);
+        console.log(metadata);
+
+        if (draggables_count!=droppables_count) {
+            if (debugging) { console.log("Ο αριθμός των draggables δεν είναι ίδιος με τον αριθμό των droppables"); }
+            alert("Fatal error #001");
+            return -1;
+        }
+        if (draggables_count>5) {
+            if (debugging) { console.log("Δεν μπορείς να έχεις περισσότερα από 5 στοιχεία σε κάθε άσκηση αντιστοίχισης"); }
+            alert("Fatal error #002");
+            fail;
+            return -2;
+            
+        }
+        return 1;
     }   
 }
 
+function setup_matching() {
+    $("#alx_draggables").html("");
+    for (var i=0; i<draggables.length; i++) {
+        $("#alx_draggables").html($("#alx_draggables").html() + draggables[i]['value']);
+    }
+
+}
 
 $( function() {
+
     if (!('id' in getUrlParams())) {
-        if (debugging)
-            console.log("TODO Δεν ορίστηκε το id του quiz - Πρέπει να τερματιστεί η εφαρμογή");
-        //return;
+        if (debugging) { console.log("TODO Δεν ορίστηκε το id του quiz - Πρέπει να τερματιστεί η εφαρμογή"); }
+        alert('Δεν ορίστηκε id');
+        return; 
     }
-    console.info("test");
+    
+
+    if (get_data_from_server()<0) {
+        if (debugging) { console.log("Σφάλμα στην ανάγνωση του json");}
+        return;
+    }
+    
+    setup_matching();
+
     $( "#draggable0" ).draggable();
     $( "#draggable1" ).draggable();
     $( "#draggable2" ).draggable();
